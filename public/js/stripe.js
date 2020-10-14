5ad9135e67b09cf4c31e893dc558614b62371f25
Yunus Emre Onal <onalllyunus@gmail.com>
2020-10-14 22:10:15 +0300

    Developer uptaded

--- public/js/stripe.js
+++ public/js/stripe.js
@@ -7,7 +7,7 @@ export const bookTour = async tourId => {
   try {
     // 1) Get checkout session from API
     const session = await axios(
-      `/api/v1/bookings/checkout-session/${tourId}`
+      `http://127.0.0.1:3000/api/v1/bookings/checkout-session/${tourId}`
     );
     // console.log(session);
 
